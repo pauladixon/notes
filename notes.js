d4e3a60814e2dc0a545f345a572efaef6225cf56
paula dixon <paulapdixon@gmail.com>
2021-01-21 13:16:37 -0800

    notes on unary/binary operators

---
+++ notes.js
@@ -0,0 +1,16 @@
+
+
+// operators
+// - modulus (%)
+// - typeof
+        console.log(typeof 4.5)
+        // → number
+        console.log(typeof "x")
+        // → string
+
+// The other operators shown all operated on two values, but typeof takes only one. Operators that use two values are called binary operators, while those that take one are called unary operators. The minus operator can be used both as a binary operator and as a unary operator.
+
+        console.log(- (10 - 2))
+        // → -8
+
+        
