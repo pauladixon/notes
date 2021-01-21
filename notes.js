ec59ab7ea5a0fb5b3f069cd62afcacf49cfe9816
paula dixon <paulapdixon@gmail.com>
2021-01-21 13:17:24 -0800

    added boolean fact

--- notes.js
+++ notes.js
@@ -13,4 +13,8 @@
         console.log(- (10 - 2))
         // → -8
 
-        
+
+// There is only one value in JavaScript that is not equal to itself, and that is NaN (“not a number”).
+
+    console.log(NaN == NaN)
+    // → false
