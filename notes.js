43ebdae9e1ed746e81ae4b2f89e259761be4d209
paula dixon <paulapdixon@gmail.com>
2021-01-25 14:22:25 -0800

    added notes for short circuit eval

--- notes.js
+++ notes.js
@@ -40,4 +40,4 @@
     console.log("Agnes" || "user")
     // → Agnes
 
-    // We can use this functionality as a way to fall back on a default value. If you have a value that might be empty, you can put || after it with a replacement value. If the initial value can be converted to false, you’ll get the replacement instead. The rules for converting strings and numbers to Boolean values state that 0, NaN, and the empty string ("") count as false, while all the other values count as true. So 0 || -1 produces -1, and "" || "!?" yields "!?"
+    // We can use this functionality as a way to fall back on a default value. If you have a value that might be empty, you can put || after it with a replacement value. If the initial value can be converted to false, you’ll get the replacement instead. The rules for converting strings and numbers to Boolean values state that 0, NaN, and the empty string ("") count as false, while all the other values count as true. So 0 || -1 produces -1, and "" || "!?" yields "!?".
