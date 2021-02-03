746986ba7eccbf8390ba87a816f19f685a754f76
paula dixon <paulapdixon@gmail.com>
2021-02-03 14:20:15 -0800

    adding more work arounds

--- notes.js
+++ notes.js
@@ -321,3 +321,19 @@
     // → 16
 
     // We defined square with only one parameter. Yet when we call it with three, the language doesn’t complain. It ignores the extra arguments and computes the square of the first one.
+
+    // JavaScript is extremely broad-minded about the number of arguments you pass to a function. If you pass too many, the extra ones are ignored. If you pass too few, the missing parameters get assigned the value undefined.
+
+    // The downside of this is that it is possible—likely, even—that you’ll accidentally pass the wrong number of arguments to functions. And no one will tell you about it.
+
+    // The upside is that this behavior can be used to allow a function to be called with different numbers of arguments. For example, this minus function tries to imitate the - operator by acting on either one or two arguments:
+
+    function minus(a, b) {
+    if (b === undefined) return -a;
+    else return a - b;
+    }
+
+    console.log(minus(10));
+    // → -10
+    console.log(minus(10, 5));
+    // → 5
