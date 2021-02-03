34021a090d04dd9b1927d34dce03d5ad2c769726
paula dixon <paulapdixon@gmail.com>
2021-02-03 14:32:24 -0800

    added notes on recursion

--- notes.js
+++ notes.js
@@ -357,3 +357,33 @@
     // → 16
     console.log(power(2, 6));
     // → 64
+
+
+// recursion
+
+    // It is perfectly okay for a function to call itself, as long as it doesn’t do it so often that it overflows the stack. A function that calls itself is called recursive.
+
+    // Consider this puzzle: by starting from the number 1 and repeatedly either adding 5 or multiplying by 3, an infinite set of numbers can be produced. How would you write a function that, given a number, tries to find a sequence of such additions and multiplications that produces that number?
+
+    // For example, the number 13 could be reached by first multiplying by 3 and then adding 5 twice, whereas the number 15 cannot be reached at all.
+
+    // Here is a recursive solution:
+
+    function findSolution(target) {
+    function find(current, history) {
+        if (current == target) {
+        return history;
+        } else if (current > target) {
+        return null;
+        } else {
+        return find(current + 5, `(${history} + 5)`) ||
+                find(current * 3, `(${history} * 3)`);
+        }
+    }
+    return find(1, "1");
+    }
+
+    console.log(findSolution(24));
+    // → (((1 * 3) + 5) * 3)
+    
+    // Note that this program doesn’t necessarily find the shortest sequence of operations. It is satisfied when it finds any sequence at all.
