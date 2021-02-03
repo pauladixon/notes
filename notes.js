d11f37028b7c180228884b0883b2339bd33e04c9
paula dixon <paulapdixon@gmail.com>
2021-02-03 14:17:06 -0800

    adding comments

--- notes.js
+++ notes.js
@@ -300,7 +300,7 @@
 
     // The place where the computer stores the context of functions is the call stack. Every time a function is called, the current context is stored on top of this stack.
 
-    // Storing this stack requires space in the computer’s memory. When the stack grows too big, the computer will fail with a message like “out of stack space” or “too much recursion”. ie:
+    // Storing this stack requires space in the computer’s memory. When the stack grows too big, the computer will fail with a message like “out of stack space” or “too much recursion”. The following code illustrates this by asking the computer a really hard question that causes an infinite back-and-forth between two functions. Rather, it would be infinite, if the computer had an infinite stack. As it is, we will run out of space, or “blow the stack”.
 
     function chicken() {
         return egg();
