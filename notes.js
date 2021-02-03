aa7ec22444383bdbbc54173a7e4816af402dcc8d
paula dixon <paulapdixon@gmail.com>
2021-02-03 13:41:13 -0800

    adding nested scope

--- notes.js
+++ notes.js
@@ -234,3 +234,24 @@
     // → 1024
 
     // A return keyword without an expression after it will cause the function to return undefined.
+
+
+// nested scope and bindings
+
+    const hummus = function(factor) {
+        const ingredient = function(amount, unit, name) {
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1) {
+            unit += "s";
+        }
+        console.log(`${ingredientAmount} ${unit} ${name}`);
+        };
+        ingredient(1, "can", "chickpeas");
+        ingredient(0.25, "cup", "tahini");
+        ingredient(0.25, "cup", "lemon juice");
+        ingredient(1, "clove", "garlic");
+        ingredient(2, "tablespoon", "olive oil");
+        ingredient(0.5, "teaspoon", "cumin");
+    };
+
+    //   The code inside the ingredient function can see the factor binding from the outer function. But its local bindings, such as unit or ingredientAmount, are not visible in the outer function.
