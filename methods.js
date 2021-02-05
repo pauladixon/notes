7096d1bd776604ef20190784534a4fa5e946f808
paula dixon <paulapdixon@gmail.com>
2021-02-05 12:42:35 -0800

    moved nan method to methods file

--- methods.js
+++ methods.js
@@ -25,3 +25,49 @@
 
 
 
+// The Number.isNaN function is a standard JavaScript function that returns true only if the argument it is given is NaN. The Number function happens to return NaN when you give it a string that doesn’t represent a valid number. Thus, the condition translates to “unless theNumber is not-a-number, do this”.
+
+    // more Number.isNaN examples for reference
+
+    let test1 = 12
+
+    if (isNaN(test1)){
+    console.log('yes')
+    } else console.log('no')
+    // → no
+    
+    
+    let test2 = NaN
+    
+    if (!isNaN(test2)){
+    console.log('yes')
+    } else console.log('no')
+    // → yes
+    
+    
+    let test3 = 12
+    
+    if (!Number.isNaN(test3)){
+    console.log('yes')
+    } else console.log('no')
+    // → yes
+    
+    
+    
+    function typeOfNaN(x) {
+    if (Number.isNaN(x)) {
+        return 'Number NaN';
+    }
+    if (isNaN(x)) {
+        return 'NaN';
+    } else return x
+    }
+    
+    console.log(typeOfNaN('100F'));
+    // → "NaN"
+    
+    console.log(typeOfNaN(NaN));
+    // → "Number NaN"
+    
+    console.log(typeOfNaN(12));
+    // → "12"
