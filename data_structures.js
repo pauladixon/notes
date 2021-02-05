393272552141d6165f50cfc2de10cb94cf56c3a8
paula dixon <paulapdixon@gmail.com>
2021-02-05 12:50:13 -0800

    adding notes on dot vs bracket notation

--- data_structures.js
+++ data_structures.js
@@ -1,2 +1,11 @@
 
-// 
+// properties
+
+    // Almost all JavaScript values have properties. The exceptions are null and undefined. If you try to access a property on one of these nonvalues, you get an error.
+
+    null.length;
+    // → TypeError: null has no properties
+
+    // The two main ways to access properties in JavaScript are with a dot and with square brackets. Both value.x and value[x] access a property on value—but not necessarily the same property. The difference is in how x is interpreted. When using a dot, the word after the dot is the literal name of the property. When using square brackets, the expression between the brackets is evaluated to get the property name. Whereas value.x fetches the property of value named “x”, value[x] tries to evaluate the expression x and uses the result, converted to a string, as the property name.
+
+    // So if you know that the property you are interested in is called color, you say value.color. If you want to extract the property named by the value held in the binding i, you say value[i]. Property names are strings. They can be any string, but the dot notation works only with names that look like valid binding names. 
