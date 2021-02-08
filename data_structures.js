0873eb597782f3b336dbf2fd362d3d24f7a01a6e
paula dixon <paulapdixon@gmail.com>
2021-02-08 10:42:05 -0800

    added notes on object mutability

--- data_structures.js
+++ data_structures.js
@@ -56,4 +56,19 @@
     // Arrays, then, are just a kind of object specialized for storing sequences of things. If you evaluate typeof [], it produces "object".
 
 
-// mutability in objects /
+// mutability in objects 
+
+    let object1 = {value: 10};
+    let object2 = object1;
+    let object3 = {value: 10};
+
+    console.log(object1 == object2);
+    // → true
+    console.log(object1 == object3);
+    // → false
+
+    object1.value = 15;
+    console.log(object2.value);
+    // → 15
+    console.log(object3.value);
+    // → 10
