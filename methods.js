eace99f068a979f7fb7f03b018741c1cfa6c9545
paula dixon <paulapdixon@gmail.com>
2021-02-08 16:15:21 -0800

    adding notes on rest parameters

--- methods.js
+++ methods.js
@@ -223,11 +223,17 @@
     // To write such a function, you put three dots before the function’s last parameter, like this:
 
     function max(...numbers) {
-    let result = -Infinity;
-    for (let number of numbers) {
-        if (number > result) result = number;
-    }
-    return result;
+        let result = -Infinity;
+        for (let number of numbers) {
+            if (number > result) result = number;
+        }
+        return result;
     }
     console.log(max(4, 1, 9, -2));
     // → 9
+
+    // You can use a similar three-dot notation to call a function with an array of arguments.
+
+    let numbers = [5, 1, 7];
+    console.log(max(...numbers));
+    // → 7
