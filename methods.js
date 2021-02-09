d47164bca985d575c77b4bcc1bf280954dd99e8e
paula dixon <paulapdixon@gmail.com>
2021-02-08 16:10:20 -0800

    added split and join string methods

--- methods.js
+++ methods.js
@@ -184,8 +184,23 @@
 
     console.log("one two three".indexOf("ee"));
     // → 11
-    
+
     // The trim method removes whitespace (spaces, newlines, tabs, and similar characters) from the start and end of a string.
 
     console.log("  okay \n ".trim());
     // → okay
+
+
+    // You can split a string on every occurrence of another string with split and join it again with join.
+
+    let sentence = "Secretarybirds specialize in stomping";
+    let words = sentence.split(" ");
+    console.log(words);
+    // → ["Secretarybirds", "specialize", "in", "stomping"]
+    console.log(words.join(". "));
+    // → Secretarybirds. specialize. in. stomping
+    
+    // A string can be repeated with the repeat method, which creates a new string containing multiple copies of the original string, glued together.
+
+    console.log("LA".repeat(3));
+    // → LALALA
