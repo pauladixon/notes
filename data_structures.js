57c9438d02722d9c883cdc68cd165fbf948e2050
paula dixon <paulapdixon@gmail.com>
2021-02-09 11:40:23 -0800

    added  comments

--- data_structures.js
+++ data_structures.js
@@ -133,3 +133,5 @@
     // → Faraji
 
     // Note that if you try to destructure null or undefined, you get an error, much as you would if you directly try to access a property of those values.
+
+    /
