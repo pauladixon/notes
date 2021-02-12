3a27beb820261a2788b0cf2c26f85f1a93da07e6
paula dixon <paulapdixon@gmail.com>
2021-02-12 12:20:58 -0800

    added notes on strategy

---
+++ algorithms.js
@@ -0,0 +1,12 @@
+
+// strategy
+
+    // There is a structure to the topics that you can learn that will greatly improve your understanding if you progress through the algorithms. Here are the steps I suggest you follow:
+
+        // Learn to evaluate efficiency (complexity) of algorithms
+        // Develop a process for problem solving
+        // Learn basic algorithm patterns
+        // Learn basic data structures
+        // Put into practice
+        // Start easy and progress harder
+        // Track and measure
