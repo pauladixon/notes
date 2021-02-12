4b09ddab13c3746557c00bbf5a3402c7d9cd0fa4
paula dixon <paulapdixon@gmail.com>
2021-02-11 16:59:23 -0800

    added first control loop exercise

--- exercises.js
+++ exercises.js
@@ -1,3 +1,16 @@
 // loop exercises
 
-    
+    // Looping a triangle
+    // Write a loop that makes seven calls to console.log to output the following triangle:
+
+    // #
+    // ##
+    // ###
+    // ####
+    // #####
+    // ######
+    // #######
+
+    for (let line = '#'; line.length < 8; line += '#'){
+        console.log(line)
+    }
