aad2de60dc5c49f1ce2f91d7ec46c02c1b80fb66
paula dixon <paulapdixon@gmail.com>
2021-02-12 12:17:42 -0800

    added min function exercise

--- exercises.js
+++ exercises.js
@@ -92,3 +92,25 @@
             }
 
             console.log(board)
+
+
+// function exercises
+
+    // Minimum
+
+    // The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+
+    // my solution
+
+        let min = (num1, num2) => {
+            if (num1 < num2){
+                return num1
+            } else return num2
+        }
+
+    // ejs solution
+
+        function min(a,b){
+            if (a<b) return a
+            else return b
+        }
