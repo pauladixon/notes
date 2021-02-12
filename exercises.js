54851e37557f85b56e8ddc4d1e787d2c39ad31f6
paula dixon <paulapdixon@gmail.com>
2021-02-12 12:09:17 -0800

    added chessboard challenge

--- exercises.js
+++ exercises.js
@@ -24,22 +24,71 @@
 
         // my solution
 
-        for (let i=1; i<=100; i++){
-            if (i%15 === 0){
-                console.log('fizzbuzz')
-            } else if (i%5 === 0){
-                console.log('buzz')
-            } else if (i%3 === 0){
-                console.log('fizz')
-            } else console.log(i)
-        }
+            for (let i=1; i<=100; i++){
+                if (i%15 === 0){
+                    console.log('fizzbuzz')
+                } else if (i%5 === 0){
+                    console.log('buzz')
+                } else if (i%3 === 0){
+                    console.log('fizz')
+                } else console.log(i)
+            }
 
         // ejs solution
 
-        for (let j=1; j<=100; j++){
-            let output = ''
-            if (j%3 == 0) output += 'fizz'
-            if (j%5 == 0) output += 'buzz'
-            console.log(output || j)
-        }
+            for (let j=1; j<=100; j++){
+                let output = ''
+                if (j%3 == 0) output += 'fizz'
+                if (j%5 == 0) output += 'buzz'
+                console.log(output || j)
+            }
 
+    // chessboard
+
+        // Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+        // Passing this string to console.log should show something like this:
+
+        //  # # # #
+        // # # # # 
+        //  # # # #
+        // # # # # 
+        //  # # # #
+        // # # # # 
+        //  # # # #
+        // # # # #
+
+        // When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+
+        // my solution 
+
+            let binding = 8
+            let string = '   # # # #'
+            
+            for (i=2; i<=binding; i++){
+            if (i%2 == 0){
+                string += `
+                # # # # `
+            } else string += `
+            # # # #`
+            }
+            
+            console.log(string)
+
+        // ejs solution
+
+            let size = 8
+            let board = ''
+
+            for (let y=0; y<size; y++){
+                for (let x=0; x<size; x++){
+                    if ((x+y)%2 ==0){
+                        board += ' '
+                    } else {
+                        board += '#'
+                    }
+                }
+                board += '/n'
+            }
+
+            console.log(board)
