c84a21bcf8e443f1183cc51b0e5acf8be4be4a9b
paula dixon <paulapdixon@gmail.com>
2021-02-22 15:10:33 -0800

    added ejs solutions

--- exercises.js
+++ exercises.js
@@ -210,20 +210,56 @@
 
         // As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 
-        let range = (start, end) => {
-            let arr = []
-            for (i=start; i<=end; i++){
-              arr.push(i)
+        // my solutions
+
+            let range = (start, end) => {
+                let arr = []
+                for (i=start; i<=end; i++){
+                    arr.push(i)
+                }
+                return arr
             }
-            return arr
-        }
-          
-        let sum = (arr) => {
-            let sum = 0
-            for(i=0; i<arr.length; i++){
-                sum = sum + arr[i]
+                
+            let sum = (arr) => {
+                let sum = 0
+                for(i=0; i<arr.length; i++){
+                    sum = sum + arr[i]
+                }
+                return sum
             }
-            return sum
-        }
-        
-        console.log(sum(range(1,10)))
+            
+            let range = (start, end, step=1) => {
+                let arr = []
+                for (i=start; i<=end; i=i+step){
+                    arr.push(i)
+                }
+                return arr
+            }
+
+        // ejs solutions
+
+            function range(start, end, step = start < end ? 1 : -1) {
+                let array = []
+            
+                if (step > 0) {
+                for (let i = start; i <= end; i += step) array.push(i);
+                } else {
+                for (let i = start; i >= end; i += step) array.push(i);
+                }
+                return array
+            }
+            
+            function sum(array) {
+                let total = 0
+                for (let value of array) {
+                total += value
+                }
+                return total
+            }
+            
+            console.log(range(1, 10))
+            // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            console.log(range(5, 2, -1))
+            // → [5, 4, 3, 2]
+            console.log(sum(range(1, 10)))
+            // → 55
