f0818509a939fd2e69948d23813c1bef4a0f1e2a
paula dixon <paulapdixon@gmail.com>
2021-02-22 15:56:52 -0800

    added reversing arrays solutions

--- exercises.js
+++ exercises.js
@@ -263,3 +263,48 @@
             // → [5, 4, 3, 2]
             console.log(sum(range(1, 10)))
             // → 55
+
+
+    // reversing an array 
+
+        // Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
+        // Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
+
+        // my solution
+
+            let reverseArray = (array) => {
+                let newArray = []
+                for (let elem of array){
+                newArray.unshift(elem)
+                }
+                return newArray
+            }
+            console.log(reverseArray([1,2,3,4,5]))
+            
+            
+        // ejs solutions
+        
+            function reverseArray(array) {
+                let output = [];
+                for (let i = array.length - 1; i >= 0; i--) {
+                output.push(array[i]);
+                }
+                return output;
+            }
+            
+            function reverseArrayInPlace(array) {
+                for (let i = 0; i < Math.floor(array.length / 2); i++) {
+                let old = array[i];
+                array[i] = array[array.length - 1 - i];
+                array[array.length - 1 - i] = old;
+                }
+                return array;
+            }
+            
+            console.log(reverseArray(["A", "B", "C"]));
+            // → ["C", "B", "A"];
+            let arrayValue = [1, 2, 3, 4, 5];
+            reverseArrayInPlace(arrayValue);
+            console.log(arrayValue);
+            // → [5, 4, 3, 2, 1]
