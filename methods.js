77412cac228f7460e246f970a21b1ae8ae85cf61
paula dixon <paulapdixon@gmail.com>
2021-02-22 12:55:06 -0800

    added notes on string methods

--- methods.js
+++ methods.js
@@ -216,6 +216,9 @@
     // → b
 
 
+    // You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+
 // rest parameters
 
     // Math.max computes the maximum of all the arguments it is given.
