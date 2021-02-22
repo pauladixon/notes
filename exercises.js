50bc8ea73246ab60b07a1ef22b934a8670cd9a2b
paula dixon <paulapdixon@gmail.com>
2021-02-22 14:26:48 -0800

    added bean counting exercise and solutions

--- exercises.js
+++ exercises.js
@@ -142,4 +142,50 @@
             console.log(isEven(75));
             // → false
             console.log(isEven(-1));
-            // → false
+            // → 
+            
+    
+    // bean counting
+
+        // my solutions
+
+            let countBs = (str) => {
+                let counter = 0
+                for (i=0; i<str.length; i++){
+                    if (str[i] === 'B'){
+                        counter++
+                    } 
+                }
+                return counter
+            }
+
+            let countChar = (str, char) => {
+                let counter = 0
+                for (i=0; i<str.length; i++){
+                    if (str[i] === char){
+                        counter++
+                    } 
+                }
+                return counter
+            }
+
+        // ejs solutions
+
+            function countChar(string, ch) {
+                let counted = 0;
+                for (let i = 0; i < string.length; i++) {
+                if (string[i] == ch) {
+                    counted += 1;
+                }
+                }
+                return counted;
+            }
+            
+            function countBs(string) {
+                return countChar(string, "B");
+            }
+            
+            console.log(countBs("BBC"));
+            // → 2
+            console.log(countChar("kakkerlak", "k"));
+            // → 4
