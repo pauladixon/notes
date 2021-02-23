d54671de427503db3ee60123a06f43b1625654d3
paula dixon <paulapdixon@gmail.com>
2021-02-22 16:59:30 -0800

    digging into objects as lists

--- exercises.js
+++ exercises.js
@@ -198,7 +198,7 @@
 
 
 
-// data structures exercises
+// array exercises
 
     // sum of a range
 
@@ -284,7 +284,7 @@
             
             
         // ejs solutions
-        
+
             function reverseArray(array) {
                 let output = [];
                 for (let i = array.length - 1; i >= 0; i--) {
@@ -308,3 +308,62 @@
             reverseArrayInPlace(arrayValue);
             console.log(arrayValue);
             // → [5, 4, 3, 2, 1]
+
+
+// object exercises
+
+    // a list
+
+        // A common data structure is the list (not to be confused with array). A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
+
+            let list = {
+                value: 1,
+                rest: {
+                value: 2,
+                rest: {
+                    value: 3,
+                    rest: null
+                }
+                }
+            };
+
+        // A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the binding defined earlier), they are both independent lists, but they share the structure that makes up their last three elements. The original list is also still a valid three-element list.
+
+        // Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+        // ejs solutions
+
+            function arrayToList(array) {
+                let list = null;
+                for (let i = array.length - 1; i >= 0; i--) {
+                list = {value: array[i], rest: list};
+                }
+                return list;
+            }
+            
+            function listToArray(list) {
+                let array = [];
+                for (let node = list; node; node = node.rest) {
+                array.push(node.value);
+                }
+                return array;
+            }
+            
+            function prepend(value, list) {
+                return {value, rest: list};
+            }
+            
+            function nth(list, n) {
+                if (!list) return undefined;
+                else if (n == 0) return list.value;
+                else return nth(list.rest, n - 1);
+            }
+            
+            console.log(arrayToList([10, 20]));
+            // → {value: 10, rest: {value: 20, rest: null}}
+            console.log(listToArray(arrayToList([10, 20, 30])));
+            // → [10, 20, 30]
+            console.log(prepend(10, prepend(20, null)));
+            // → {value: 10, rest: {value: 20, rest: null}}
+            console.log(nth(arrayToList([10, 20, 30]), 1));
+            // → 20
