daa794ec61cda8bf9122b50e3d24444d0eebe297
paula dixon <paulapdixon@gmail.com>
2021-02-23 17:05:23 -0800

    added notes on function closures

--- functions.js
+++ functions.js
@@ -193,3 +193,26 @@
 
     // Still, there’s no need to feel bad when writing functions that are not pure or to wage a holy war to purge them from your code. Side effects are often useful. There’d be no way to write a pure version of console.log, for example, and console.log is good to have. Some operations are also easier to express in an efficient way when we use side effects, so computing speed can be a reason to avoid purity.
 
+
+// closures
+
+    // method for keeping a value connected to a function 
+
+        function handleLikePost(step) {
+            let likeCount = 0;
+            return function addLike() {
+            likeCount += step;    
+            return likeCount;
+            }
+        //   addLike();
+            console.log('like count:', likeCount);
+        }
+        
+        const doubleLike = handleLikePost(2);
+        
+        console.log(doubleLike());
+        console.log(doubleLike());
+        console.log(doubleLike());
+        
+        // 1) Closures are a property of JavaScript functions
+        // 2) Call function in different scope than where function was original defined
