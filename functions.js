9dc24e7d388eca4df0802acc8f2662c0e74611b9
paula dixon <paulapdixon@gmail.com>
2021-02-24 10:02:04 -0800

    cleaning up code

--- functions.js
+++ functions.js
@@ -93,6 +93,16 @@
         // A less verbose function value
             let h = a => a % 3;
 
+    
+    // more examples
+
+        const username = 'john';
+
+        const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+        
+        console.log(capitalize(username));
+    
+
 
 // notes on call stacks
 
@@ -242,3 +252,18 @@
           console.log(countingDown());
           console.log(countingDown());
           
+
+
+// callback functions
+
+    const username = 'john'
+
+    const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`
+    
+    function greetUser(name, callback) {
+    return callback(capitalize(name))
+    }
+
+    const result = greetUser(username, name => `Hi there, ${name}!`)
+
+    console.log(result)
