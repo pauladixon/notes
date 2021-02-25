03bb1e0ac3523f3f58ca73784fa9424fd851760f
paula dixon <paulapdixon@gmail.com>
2021-02-25 12:18:10 -0800

    cleaning up code

--- methods.js
+++ methods.js
@@ -149,9 +149,9 @@
 
     // Another fundamental array method is slice, which takes start and end indices and returns an array that has only the elements between them. The start index is inclusive, the end index exclusive.
 
-    console.log([0, 1, 2, 3, 4].slice(2, 4));
+    console.log([0, 1, 2, 3, 4].slice(2, 4))
     // → [2, 3]
-    console.log([0, 1, 2, 3, 4].slice(2));
+    console.log([0, 1, 2, 3, 4].slice(2))
     // → [2, 3, 4]
 
     // When the end index is not given, slice will take all of the elements after the start index. You can also omit the start index to copy the entire array.
@@ -165,9 +165,9 @@
 
     function remove(array, index) {
     return array.slice(0, index)
-        .concat(array.slice(index + 1));
+        .concat(array.slice(index + 1))
     }
-    console.log(remove(["a", "b", "c", "d", "e"], 2));
+    console.log(remove(["a", "b", "c", "d", "e"], 2))
     // → ["a", "b", "d", "e"]
 
     // If you pass concat an argument that is not an array, that value will be added to the new array as if it were a one-element array.
