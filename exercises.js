69d7f2a4bcdb809ea2cf22f531a26eac2453e267
paula dixon <paulapdixon@gmail.com>
2021-02-25 12:20:47 -0800

    cleaning up code

--- exercises.js
+++ exercises.js
@@ -133,17 +133,17 @@
         // ejs solution
 
             function isEven(n) {
-                if (n == 0) return true;
-                else if (n == 1) return false;
-                else if (n < 0) return isEven(-n);
-                else return isEven(n - 2);
+                if (n == 0) return true
+                else if (n == 1) return false
+                else if (n < 0) return isEven(-n)
+                else return isEven(n - 2)
             }
             
-            console.log(isEven(50));
+            console.log(isEven(50))
             // → true
-            console.log(isEven(75));
+            console.log(isEven(75))
             // → false
-            console.log(isEven(-1));
+            console.log(isEven(-1))
             // → 
             
     
