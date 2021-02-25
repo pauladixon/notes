1b7b4a67b243054dfa6a154f0614043e0d584042
paula dixon <paulapdixon@gmail.com>
2021-02-25 12:26:30 -0800

    cleaning up code

--- exercises.js
+++ exercises.js
@@ -381,26 +381,22 @@
         
 
         function deepEqual(a, b) {
-            if (a === b) return true;
+            if (a === b) return true
+            if (a == null || typeof a != "object" || b == null || typeof b != "object") return false
             
-            if (a == null || typeof a != "object" ||
-                b == null || typeof b != "object") return false;
-          
-            let keysA = Object.keys(a), keysB = Object.keys(b);
-          
-            if (keysA.length != keysB.length) return false;
-          
+            let keysA = Object.keys(a), keysB = Object.keys(b)
+            if (keysA.length != keysB.length) return false
+            
             for (let key of keysA) {
-              if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+                if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false
             }
+            return true
+        }
           
-            return true;
-          }
-          
-          let obj = {here: {is: "an"}, object: 2};
-          console.log(deepEqual(obj, obj));
+        let obj = {here: {is: "an"}, object: 2}
+        console.log(deepEqual(obj, obj))
           // → true
-          console.log(deepEqual(obj, {here: 1, object: 2}));
+        console.log(deepEqual(obj, {here: 1, object: 2}))
           // → false
-          console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+        console.log(deepEqual(obj, {here: {is: "an"}, object: 2}))
           // → true
