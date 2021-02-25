8ab604561a578f68cffeee50f898f9cfcc7f3897
paula dixon <paulapdixon@gmail.com>
2021-02-25 12:01:42 -0800

    cleaning up code

--- functions.js
+++ functions.js
@@ -82,25 +82,25 @@
 
         // Define f to hold a function value
             const f = function(a) {
-                console.log(a + 2);
-            };
+                console.log(a + 2)
+            }
           
         // Declare g to be a function
             function g(a, b) {
-                return a * b * 3.5;
+                return a * b * 3.5
             }
           
         // A less verbose function value
-            let h = a => a % 3;
+            let h = a => a % 3
 
     
     // more examples
 
-        const username = 'john';
+        const username = 'john'
 
-        const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+        const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`; 
         
-        console.log(capitalize(username));
+        console.log(capitalize(username))
     
 
 
@@ -111,12 +111,12 @@
     // Storing this stack requires space in the computer’s memory. When the stack grows too big, the computer will fail with a message like “out of stack space” or “too much recursion”. The following code illustrates this by asking the computer a really hard question that causes an infinite back-and-forth between two functions. Rather, it would be infinite, if the computer had an infinite stack. As it is, we will run out of space, or “blow the stack”.
 
     function chicken() {
-        return egg();
+        return egg()
     }
     function egg() {
-        return chicken();
+        return chicken()
     }
-    console.log(chicken() + " came first.");
+    console.log(chicken() + " came first.")
     // → ??
 
 
@@ -125,7 +125,7 @@
     // The following code is allowed and executes without any problem:
 
     function square(x) { return x * x; }
-    console.log(square(4, true, "hedgehog"));
+    console.log(square(4, true, "hedgehog"))
     // → 16
 
     // We defined square with only one parameter. Yet when we call it with three, the language doesn’t complain. It ignores the extra arguments and computes the square of the first one.
@@ -137,13 +137,13 @@
     // The upside is that this behavior can be used to allow a function to be called with different numbers of arguments. For example, this minus function tries to imitate the - operator by acting on either one or two arguments:
 
     function minus(a, b) {
-    if (b === undefined) return -a;
-    else return a - b;
+        if (b === undefined) return -a
+        else return a - b
     }
 
-    console.log(minus(10));
+    console.log(minus(10))
     // → -10
-    console.log(minus(10, 5));
+    console.log(minus(10, 5))
     // → 5
 
 
@@ -154,28 +154,28 @@
     // For example, this version of power makes its second argument optional. If you don’t provide it or pass the value undefined, it will default to two, and the function will behave like square.
 
         function power(base, exponent = 2) {
-        let result = 1;
-        for (let count = 0; count < exponent; count++) {
-            result *= base;
-        }
-        return result;
+            let result = 1
+            for (let count = 0; count < exponent; count++) {
+                result *= base
+            }
+            return result
         }
 
-        console.log(power(4));
+        console.log(power(4))
         // → 16
-        console.log(power(2, 6));
+        console.log(power(2, 6))
         // → 64
 
     // can limit the parameters inside a function to make sure results are within the scope desired, ie. significant digits
 
         function convertTemperature(celsius, decimalPlaces = 1) {
             // celsius to fahrenheit
-        //   decimalPlaces = decimalPlaces || 1;
-        const fahrenheit = celsius * 1.8 + 32;
-        return Number(fahrenheit.toFixed(decimalPlaces));
+            //   decimalPlaces = decimalPlaces || 1;
+            const fahrenheit = celsius * 1.8 + 32;
+            return Number(fahrenheit.toFixed(decimalPlaces))
         }
         
-        console.log(convertTemperature(21, 0));
+        console.log(convertTemperature(21, 0))
 
 
 // recursion
@@ -188,22 +188,21 @@
 
     // Here is a recursive solution:
 
-    function findSolution(target) {
-    function find(current, history) {
-        if (current == target) {
-        return history;
-        } else if (current > target) {
-        return null;
-        } else {
-        return find(current + 5, `(${history} + 5)`) ||
-                find(current * 3, `(${history} * 3)`);
+        function findSolution(target) {
+            function find(current, history) {
+                if (current == target) {
+                    return history
+                } else if (current > target) {
+                    return null
+                } else {
+                    return find(current + 5, `(${history} + 5)`) || find(current * 3, `(${history} * 3)`)
+                }
+            }
+            return find(1, "1")
         }
-    }
-    return find(1, "1");
-    }
 
-    console.log(findSolution(24));
-    // → (((1 * 3) + 5) * 3)
+        console.log(findSolution(24));
+        // → (((1 * 3) + 5) * 3)
 
     // Note that this program doesn’t necessarily find the shortest sequence of operations. It is satisfied when it finds any sequence at all.
 
@@ -225,14 +224,14 @@
                 likeCount += step
                 return likeCount
             }
-            console.log('like count:', likeCount);
+            console.log('like count:', likeCount)
         }
         
-        const doubleLike = handleLikePost(2);
+        const doubleLike = handleLikePost(2)
         
-        console.log(doubleLike());
-        console.log(doubleLike());
-        console.log(doubleLike());
+        console.log(doubleLike())
+        console.log(doubleLike())
+        console.log(doubleLike())
         
         // 1) Closures are a property of JavaScript functions
         // 2) Call function in different scope than where function was original defined
@@ -241,17 +240,17 @@
         function countdown() {
             let number = 10
             return function removeNumber() {
-              number --
-              return number
+                number --
+                return number
             }
-          }
+        }
           
-          const countingDown = countdown();
-          
-          console.log(countingDown());
-          console.log(countingDown());
-          console.log(countingDown());
-          
+        const countingDown = countdown()
+        
+        console.log(countingDown())
+        console.log(countingDown())
+        console.log(countingDown())
+        
 
 
 // callback functions
@@ -261,7 +260,7 @@
     const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`
     
     function greetUser(name, callback) {
-    return callback(capitalize(name))
+        return callback(capitalize(name))
     }
 
     const result = greetUser(username, name => `Hi there, ${name}!`)
