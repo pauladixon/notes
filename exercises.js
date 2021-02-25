5415d8a39dc5da95cf4e0f31646d55c0ba1ff6b3
paula dixon <paulapdixon@gmail.com>
2021-02-25 12:24:33 -0800

    cleaning up code

--- exercises.js
+++ exercises.js
@@ -321,9 +321,9 @@
                 rest: {
                 value: 2,
                 rest: {
-                    value: 3,
-                    rest: null
-                }
+                        value: 3,
+                        rest: null
+                    }
                 }
             }
 
@@ -334,38 +334,38 @@
         // ejs solutions
 
             function arrayToList(array) {
-                let list = null;
+                let list = null
                 for (let i = array.length - 1; i >= 0; i--) {
-                list = {value: array[i], rest: list};
+                    list = {value: array[i], rest: list}
                 }
-                return list;
+                return list
             }
             
             function listToArray(list) {
-                let array = [];
+                let array = []
                 for (let node = list; node; node = node.rest) {
-                array.push(node.value);
+                    array.push(node.value)
                 }
-                return array;
+                return array
             }
             
             function prepend(value, list) {
-                return {value, rest: list};
+                return {value, rest: list}
             }
             
             function nth(list, n) {
-                if (!list) return undefined;
-                else if (n == 0) return list.value;
-                else return nth(list.rest, n - 1);
+                if (!list) return undefined
+                else if (n == 0) return list.value
+                else return nth(list.rest, n - 1)
             }
             
-            console.log(arrayToList([10, 20]));
+            console.log(arrayToList([10, 20]))
             // → {value: 10, rest: {value: 20, rest: null}}
-            console.log(listToArray(arrayToList([10, 20, 30])));
+            console.log(listToArray(arrayToList([10, 20, 30])))
             // → [10, 20, 30]
-            console.log(prepend(10, prepend(20, null)));
+            console.log(prepend(10, prepend(20, null)))
             // → {value: 10, rest: {value: 20, rest: null}}
-            console.log(nth(arrayToList([10, 20, 30]), 1));
+            console.log(nth(arrayToList([10, 20, 30]), 1))
             // → 20
 
 
