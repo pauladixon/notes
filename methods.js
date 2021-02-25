89944afd7fb37b84a9e1990f520a8fadc3a89ac7
paula dixon <paulapdixon@gmail.com>
2021-02-25 12:18:38 -0800

    cleaning up code

--- methods.js
+++ methods.js
@@ -175,34 +175,34 @@
 
 // string methods
         
-    console.log("coconuts".slice(4, 7));
+    console.log("coconuts".slice(4, 7))
     // → nut
-    console.log("coconut".indexOf("u"));
+    console.log("coconut".indexOf("u"))
     // → 5
     
     // One difference is that a string’s indexOf can search for a string containing more than one character, whereas the corresponding array method looks only for a single element.
 
-    console.log("one two three".indexOf("ee"));
+    console.log("one two three".indexOf("ee"))
     // → 11
 
     // The trim method removes whitespace (spaces, newlines, tabs, and similar characters) from the start and end of a string.
 
-    console.log("  okay \n ".trim());
+    console.log("  okay \n ".trim())
     // → okay
 
 
     // You can split a string on every occurrence of another string with split and join it again with join.
 
-    let sentence = "Secretarybirds specialize in stomping";
-    let words = sentence.split(" ");
-    console.log(words);
+    let sentence = "Secretarybirds specialize in stomping"
+    let words = sentence.split(" ")
+    console.log(words)
     // → ["Secretarybirds", "specialize", "in", "stomping"]
-    console.log(words.join(". "));
+    console.log(words.join(". "))
     // → Secretarybirds. specialize. in. stomping
 
     // A string can be repeated with the repeat method, which creates a new string containing multiple copies of the original string, glued together.
 
-    console.log("LA".repeat(3));
+    console.log("LA".repeat(3))
     // → LALALA
 
     // assessing strings:
