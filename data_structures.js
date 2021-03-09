59f145de58ffaed5d12b9e2494149e6b5039770d
paula dixon <paulapdixon@gmail.com>
2021-03-08 16:32:56 -0800

    removed semicolons

--- data_structures.js
+++ data_structures.js
@@ -144,13 +144,13 @@
         details: {
         title: "Programmer"  
         }  
-    };
+    }
     
     // const { title } = user.details
     // const { name, details: { title} } = user;
     
     function displayUserBio({ name, details: { title} }) {
-        console.log(`${name} is a ${title}`); 
+        console.log(`${name} is a ${title}`)
     }
     
-    displayUserBio(user);
+    displayUserBio(user)
