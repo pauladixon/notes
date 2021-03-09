c5ad35d7b1b3fbb1c26c5f9f6af93988ea03fff9
paula dixon <paulapdixon@gmail.com>
2021-03-09 12:37:51 -0800

    cleaning up code

--- exercises.js
+++ exercises.js
@@ -460,12 +460,12 @@
                 }
             
             // recursive solution
-            
-            let tribonacci = (n) => {
-                if (n===0) return 0
-                else if (n===1 || n===2) return 1
-                else return tribonacci(n-3) + tribonacci(n-2) + tribonacci(n-1)
-            }
+
+                let tribonacci = (n) => {
+                    if (n===0) return 0
+                    else if (n===1 || n===2) return 1
+                    else return tribonacci(n-3) + tribonacci(n-2) + tribonacci(n-1)
+                }
 
     // climbing stairs
 
