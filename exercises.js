e837bc125f36af0a9f3975c251b6e30412ed1cb9
paula dixon <paulapdixon@gmail.com>
2021-03-09 12:36:52 -0800

    cleaning up code

--- exercises.js
+++ exercises.js
@@ -469,10 +469,10 @@
 
         // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
-        var climbStairs = function(n) {
-            if (n<=2){
-                return n
-            } else {
-                return climbStairs(n-1) + climbStairs(n-2)
+            var climbStairs = function(n) {
+                if (n<=2){
+                    return n
+                } else {
+                    return climbStairs(n-1) + climbStairs(n-2)
+                }
             }
-        }
