3895228c7465218fbb8c9072b379a7b2c703b7d0
paula dixon <paulapdixon@gmail.com>
2021-03-11 13:23:28 -0800

    added notes on reduce method

--- methods.js
+++ methods.js
@@ -389,4 +389,22 @@
     
 // reduce method
 
+    // always return the accumulator
+
+    const menuItems = [
+        { item: "Blue Cheese Salad", price: 8 },
+        { item: "Spicy Chicken Rigatoni", price: 18 },
+        { item: "Ponzu Glazed Salmon", price: 23 },
+        { item: "Philly Cheese Steak", price: 13 },
+        { item: "Baked Italian Chicken Sub", price: 12 },
+        { item: "Pan Seared Ribeye", price: 31 }
+    ]
+    
+    let result = menuItems.reduce((accumulator, menuItem) => {
+        accumulator = accumulator + menuItem.price
+        return accumulator
+    }, 0)
+    
+    console.log(result)
+
     
