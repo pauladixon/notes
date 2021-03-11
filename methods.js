12a2cb5bb3426ca1ddfb3925aea8ecd7b2913859
paula dixon <paulapdixon@gmail.com>
2021-03-11 13:02:21 -0800

    added filter method

--- methods.js
+++ methods.js
@@ -360,3 +360,20 @@
             }
         })
            
+
+// filter method for arrays
+
+    // mutates array, doesn't change the original
+
+        const restaurants = [
+            { name: 'Cap City Diner', milesAway: 2.2 },
+            { name: 'Chop Shop', milesAway: 4.1 },
+            { name: 'Northstar Cafe', milesAway: 0.9 },
+            { name: 'City Tavern', milesAway: 0.5 },
+            { name: 'Shake Shack', milesAway: 5.3 }
+        ]
+        
+        const results = restaurants.filter(restaurant => restaurant.name.startsWith('C'))
+        console.log(results)
+
+    
