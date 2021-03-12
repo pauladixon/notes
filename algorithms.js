08b0bb11f777eb0dec22fca68332be1354d0b66b
paula dixon <paulapdixon@gmail.com>
2021-03-12 12:38:14 -0800

    added reverse linked list to algorithms

--- algorithms.js
+++ algorithms.js
@@ -13,6 +13,25 @@
 
 
 
-        
+// linked lists
 
-        
+    // reverse the linked list 
+
+        // Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+        // Example 1:    
+            // Input: head = [1,2,3,4,5]
+            // Output: [5,4,3,2,1]
+            
+            var reverseList = function(head) {
+                let prevNode = null
+                
+                while (head !== null){
+                    let nextNode = head.next
+                    head.next = prevNode
+                    prevNode = head
+                    head = nextNode
+                }
+                return prevNode
+                
+            }
