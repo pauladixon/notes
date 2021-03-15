c1162baccef1275b728e23b19148637092fa847e
paula dixon <paulapdixon@gmail.com>
2021-03-15 12:37:51 -0700

    adding notes on rest operator

--- methods.js
+++ methods.js
@@ -225,25 +225,40 @@
 
     // To write such a function, you put three dots before the function’s last parameter, like this:
 
-    function max(...numbers) {
-        let result = -Infinity
-        for (let number of numbers) {
-            if (number > result) result = number
+        function max(...numbers) {
+            let result = -Infinity
+            for (let number of numbers) {
+                if (number > result) result = number
+            }
+            return result
         }
-        return result
-    }
-    console.log(max(4, 1, 9, -2))
-    // → 9
+        console.log(max(4, 1, 9, -2))
+        // → 9
 
     // You can use a similar three-dot notation to call a function with an array of arguments.
 
-    let numbers = [5, 1, 7]
-    console.log(max(...numbers))
-    // → 7
+        let numbers = [5, 1, 7]
+        console.log(max(...numbers))
+        // → 7
 
-    let words = ["never", "fully"]
-    console.log(["will", ...words, "understand"])
-    // → ["will", "never", "fully", "understand"]
+        let words = ["never", "fully"]
+        console.log(["will", ...words, "understand"])
+        // → ["will", "never", "fully", "understand"]
+
+
+    // use to destructure array
+
+
+        const finalMenuItems = [
+            "American Cheeseburger",
+            "Southern Fried Chicken",
+            "Glazed Salmon"
+        ]
+        
+        const [winner, ...losers] = finalMenuItems
+        
+        console.log({ winner, losers })
+        // -> {winner: "American Cheeseburger", losers: ["Southern Fried Chicken", "Glazed Salmon"]}
 
 
 // the math object 
