8a663a5fc86b2f46ff3b74bfac4d0bf69b24400a
paula dixon <paulapdixon@gmail.com>
2021-03-15 13:05:42 -0700

    cleaning up code

--- data_structures.js
+++ data_structures.js
@@ -297,7 +297,7 @@
         const obj = { one: 1, two: 2 };
 
         for (const key in obj) {
-        console.log('value', obj[key]);
+            console.log('value', obj[key]);
         }
 
 
@@ -355,26 +355,27 @@
 
             const users = {
                 '2345234': {
-                name: "John",
-                age: 29
+                    name: "John",
+                    age: 29
                 },
                 '8798129': {
-                name: "Jane",
-                age: 42
+                    name: "Jane",
+                    age: 42
                 },
                 '1092384': {
-                name: "Fred",
-                age: 17 
+                    name: "Fred",
+                    age: 17 
                 }
-            };
+            }
             
             const usersOver20 = Object.entries(users).reduce((acc, [id, user]) => {
-                if (user.age > 20) {
-                acc.push({ ...user, id });
+                    if (user.age > 20) {
+                    acc.push({ ...user, id })
                 }  
-                return acc;
-            }, []);
-            console.log(usersOver20);
+                return acc
+            }, [])
+
+            console.log(usersOver20)
             // => [{name: "John", age: 29, id: "2345234"}, {name: "Jane", age: 42, id: "8798129"}]
 
           
