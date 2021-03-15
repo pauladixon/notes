87b6fc4696a93146d3fdf25120b299d968f9c1e0
paula dixon <paulapdixon@gmail.com>
2021-03-15 13:04:46 -0700

    adding more object.entries reduce chains

--- data_structures.js
+++ data_structures.js
@@ -351,4 +351,30 @@
         console.log(Object.defineProperties(user))
         // => [['name', 'john'], ['age', 29]]
 
-        
+        // chaining reduce
+
+            const users = {
+                '2345234': {
+                name: "John",
+                age: 29
+                },
+                '8798129': {
+                name: "Jane",
+                age: 42
+                },
+                '1092384': {
+                name: "Fred",
+                age: 17 
+                }
+            };
+            
+            const usersOver20 = Object.entries(users).reduce((acc, [id, user]) => {
+                if (user.age > 20) {
+                acc.push({ ...user, id });
+                }  
+                return acc;
+            }, []);
+            console.log(usersOver20);
+            // => [{name: "John", age: 29, id: "2345234"}, {name: "Jane", age: 42, id: "8798129"}]
+
+          
